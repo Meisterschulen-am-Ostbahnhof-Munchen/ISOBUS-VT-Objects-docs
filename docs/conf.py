e39e6a00fe5be2729766d94059bbcfbcdb662829
Franz Höpfinger <franz.hoepfinger@ms-muc.de>
2022-09-18 17:56:39 +0200

    ..

--- docs/conf.py
+++ docs/conf.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # Configuration file for the Sphinx documentation builder.
 #
 # This file only contains a selection of the most common options. For a full
@@ -28,10 +29,11 @@ author = "Franz Höpfinger"
 extensions = [
     "sphinx.ext.duration",
     "sphinx.ext.doctest",
-	"sphinx.ext.autosectionlabel",
+    "sphinx.ext.autosectionlabel",
     "sphinx.ext.autodoc",
     "sphinx.ext.intersphinx",
     "sphinx.ext.imgconverter",
+    "recommonmark"
 ]
 
 version = "0.0.1"
@@ -63,9 +65,5 @@ html_theme = "sphinx_rtd_theme"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
-
-html_theme_options = {
-    'navigation_depth': 9,
-}
+html_static_path = ['_static']
 

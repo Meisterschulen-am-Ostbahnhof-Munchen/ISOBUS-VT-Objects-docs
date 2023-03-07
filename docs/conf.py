7dfe3e6e0e2b2cbfdd9b662f98f56cfd2c1ffade
Franz Höpfinger <franz.hoepfinger@ms-muc.de>
2023-03-08 00:10:32 +0100

    py

--- docs/conf.py
+++ docs/conf.py
@@ -1,52 +1,22 @@
-# -*- coding: utf-8 -*-
 # Configuration file for the Sphinx documentation builder.
 #
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
+# For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
-
-
 # -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = "ISOBUS-VT-Objects Docs"
 copyright = "2022, Meisterschulen am Ostbahnhof, München"
 author = "Franz Höpfinger"
 
-
 # -- General configuration ---------------------------------------------------
-# -- General configuration
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = [
-    "myst_parser",
-    "sphinx.ext.duration",
-    "sphinx.ext.doctest",
-    "sphinx.ext.autosectionlabel",
-    "sphinx.ext.autodoc",
-    "sphinx.ext.intersphinx",
-    "sphinx.ext.imgconverter",
-    "sphinxcontrib.mermaid",
-    "sphinx_markdown_tables"
-]
+extensions = ["myst_parser"]
 
 version = "0.0.1"
 release = version
-intersphinx_mapping = {
-    "rtd": ("https://docs.readthedocs.io/en/stable/", None),
-    "python": ("https://docs.python.org/3/", None),
-    "sphinx": ("https://www.sphinx-doc.org/en/master/", None),
-}
-intersphinx_disabled_domains = ["std"]
-
 templates_path = ["_templates"]
 
 # -- Options for EPUB output
@@ -57,7 +27,17 @@ epub_show_urls = "footnote"
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
+
+# -- MyST settings ---------------------------------------------------
+
+myst_enable_extensions = [
+    "colon_fence",
+]
+myst_heading_anchors = 7
+
+
 # -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
@@ -67,5 +47,4 @@ html_theme = "sphinx_rtd_theme"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
-
+html_static_path = ["_static"]
